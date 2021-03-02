176ab97628b3f9dddf7b27620e95e967cb851e93
Tom Taylor <tom@tomxxi.com>
2021-03-02 22:53:03 +0000

    Implemented Windows functionality
    Due to constraints with webusb on Windows platforms, the software now uses the normal TegraSmash method that most loaders do.. When on Windows. With this commit, Windows functionality fully works.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,83 @@
-const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain, shell } = require('electron');
 const fs = require('fs')
 const path = require('path');
-const USB = require("WEBUSB").usb;
+const url = require('url');
+
+// Global developer mode toggle.
+var devMode = false;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+    app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 960,
-    height: 650,
-    webPreferences: {
-      nodeIntegration: true
+    if (devMode) {
+        var width = 960;
+        var height = 650;
+    } else {
+        var width = 750;
+        var height = 650;
     }
-  });
 
-  // Remove the application menu.
-  mainWindow.removeMenu();
+    // const iconUrl = url.format({
+    //     pathname: path.join(__dirname, 'icons/mac/icon.icns'),
+    //     protocol: 'file: ',
+    //     slashes: true
+    // })
+    // console.log(iconUrl);
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: width,
+        height: height,
+        //icon: iconUrl,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Remove the application menu.
+    mainWindow.removeMenu();
+
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    // Open the DevTools.
+    if (devMode) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    // Redirect internal URLs to an external browser.
+    mainWindow.webContents.on('new-window', function (event, url) {
+        event.preventDefault();
+        shell.openExternal(url);
+    });
 };
 
-ipcMain.on('show-open-dialog', (event, arg)=> {
-  const window = BrowserWindow.getFocusedWindow();
-  const options = {
-      title: 'Select the payload file',
-      //defaultPath: '/path/to/something/',
-      //buttonLabel: 'Do it',
-       filters: [
-         { name: 'Payload file', extensions: ['bin'] }
-       ],
-      //message: 'This message will only be shown on macOS'
+ipcMain.on('show-open-dialog', (event, arg) => {
+    const window = BrowserWindow.getFocusedWindow();
+    const options = {
+        title: 'Select the payload file',
+        //defaultPath: '/path/to/something/',
+        //buttonLabel: 'Do it',
+        filters: [
+            { name: 'Payload file', extensions: ['bin'] }
+        ],
+        //message: 'This message will only be shown on macOS'
     };
 
     dialog.showOpenDialog(window, options).then
-    (
-        result => {
-            if (!result.canceled)
-            {
-                let paths = result.filePaths;
-                if (paths && paths.length > 0) {
-                    window.webContents.send('fileSelected', paths[0]);
+        (
+            result => {
+                if (!result.canceled) {
+                    let paths = result.filePaths;
+                    if (paths && paths.length > 0) {
+                        window.webContents.send('fileSelected', paths[0]);
+                    }
                 }
             }
-        }
-    );
+        );
 })
 
 // This method will be called when Electron has finished
@@ -63,17 +89,17 @@ app.on('ready', createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
--- src/switch.js
+++ src/switch.js
@@ -5,28 +5,130 @@ const INTERMEZZO = new Uint8Array([
     0x20, 0x00, 0x9F, 0xE5, 0x10, 0xFF, 0x2F, 0xE1, 0x04, 0x30, 0x90, 0xE4, 0x04, 0x30, 0x81, 0xE4,
     0x04, 0x20, 0x52, 0xE2, 0xFB, 0xFF, 0xFF, 0x1A, 0x1E, 0xFF, 0x2F, 0xE1, 0x20, 0xF0, 0x01, 0x40,
     0x5C, 0xF0, 0x01, 0x40, 0x00, 0x00, 0x02, 0x40, 0x00, 0x00, 0x01, 0x40
-  ]);
+]);
 
 const RCM_PAYLOAD_ADDRESS = 0x40010000;
 const INTERMEZZO_LOCATION = 0x4001F000;
-const PAYLOAD_LOAD_BLOCK = 0x40020000;
 
 var payloadPath = '';
 var device = null;
 
 var currentStep = 1;
 
-// HELPERS
+// HELPERS ----------------------------------------------
+function create_auto_device_find_event() {
+    clearInterval(create_auto_device_find_event);
+    const interval = setInterval(function () {
+        if (validateDevice()) {
+            clearInterval(create_auto_device_find_event);
+            return;
+        }
 
-// function readFileAsArrayBuffer(file) {
-//     return new Promise((res, rej) => {
-//         const reader = new FileReader();
-//         reader.onload = e => {
-//         res(e.target.result);
-//     }
-//     reader.readAsArrayBuffer(file);
-//     });
-// }
+        loadDevice();
+    }, 1000);
+}
+
+function reset() {
+    device = null;
+    payloadPath = '';
+    updateSteps();
+    create_auto_device_find_event();
+}
+
+function smashCompleteDialog(success) {
+    reset();
+
+    const Swal = require('sweetalert2');
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 5000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    });
+
+    if (success) {
+        title = 'Payload delivered successfully! 💌';
+        titleHTML = '<a class="nouserselect" style="color:var(--title-text-color);">' + title + '</a>';
+        console.log(title);
+        Toast.fire({
+            icon: 'success',
+            title: titleHTML,
+            background: 'var(--main-background-color)'
+        });
+    } else {
+        title = 'Payload delivery to the Switch failed 🍐';
+        titleHTML = '<a class="nouserselect" style="color:var(--title-text-color);">' + title + '</a>';
+        console.log(title);
+        Toast.fire({
+            icon: 'error',
+            title: titleHTML,
+            background: 'var(--main-background-color)'
+        });
+    }
+}
+
+function doWindowsDriverCheck() {
+    const os = require('os');
+    if ((os.type() == 'Windows_NT') && (!validateDevice())) {
+        const Swal = require('sweetalert2');
+        Swal.fire({
+            title: '<a class="nouserselect" style="color:var(--title-text-color);">Have you installed the driver?</a>',
+            html: "<a class='nouserselect' style='color:var(--subtitle-text-color);'>On Windows you have to install a driver to talk to the Switch. If you haven't already, make sure to install it now. It's very simple.</a>",
+            //icon: 'error',
+            background: 'var(--main-background-color)',
+            confirmButtonText: '<a class="nouserselect" style="color:var(--text-color);"><b>Install driver</b></a>',
+            showConfirmButton: true,
+            cancelButtonText: "<a class='nouserselect' style='color:var(--text-color);'>It's already installed</a>",
+            showCancelButton: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const path = require('path');
+                const { exec } = require('child_process');
+
+                const driverprocess = exec(path.join(__dirname, '/apx_driver/InstallDriver.exe'), function (error, stdout, stderr) { });
+
+                driverprocess.on('exit', function (code) {
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 5000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    });
+
+                    if (code == 1) {
+                        title = 'Driver installation succeeded';
+                        titleHTML = '<a class="nouserselect" style="color:var(--title-text-color);">' + title + '</a>';
+                        console.log(title);
+                        Toast.fire({
+                            icon: 'success',
+                            title: titleHTML,
+                            background: 'var(--main-background-color)'
+                        });
+                    } else {
+                        title = 'Driver installation failed or was stopped';
+                        titleHTML = '<a class="nouserselect" style="color:var(--title-text-color);">' + title + '</a>';
+                        console.log(title);
+                        Toast.fire({
+                            icon: 'warning',
+                            title: titleHTML,
+                            background: 'var(--main-background-color)'
+                        });
+                    }
+                });
+            }
+        });
+    }
+}
 
 function bufferToHex(data) {
     let result = "";
@@ -39,19 +141,19 @@ async function write(data) {
     let length = data.length;
     let writeCount = 0;
     const packetSize = 0x1000;
-  
+
     while (length) {
-      const dataToTransmit = Math.min(length, packetSize);
-      length -= dataToTransmit;
-  
-      const chunk = data.slice(0, dataToTransmit);
-      data = data.slice(dataToTransmit);
-      await device.transferOut(1, chunk);
-      writeCount++;
+        const dataToTransmit = Math.min(length, packetSize);
+        length -= dataToTransmit;
+
+        const chunk = data.slice(0, dataToTransmit);
+        data = data.slice(dataToTransmit);
+        await device.transferOut(1, chunk);
+        writeCount++;
     }
-  
+
     return writeCount;
-  }
+}
 
 function createRCMPayload(intermezzo, payload) {
     const rcmLength = 0x30298;
@@ -66,7 +168,7 @@ function createRCMPayload(intermezzo, payload) {
 
     for (let i = 0; i < intermezzoAddressRepeatCount; i++) {
         rcmPayloadView.setUint32(0x2A8 + i * 4, INTERMEZZO_LOCATION, true);
-     }
+    }
 
     rcmPayload.set(intermezzo, 0x2A8 + (0x4 * intermezzoAddressRepeatCount));
     rcmPayload.set(payload, 0x2A8 + (0x4 * intermezzoAddressRepeatCount) + 0x1000);
@@ -84,36 +186,77 @@ function validatePayload() {
     const fs = require('fs')
     try {
         valid = (payloadPath != '') && (fs.existsSync(payloadPath));
-    } catch(err) {
+    } catch (err) {
         valid = false;
     }
-    
+
     return valid;
 }
 
 // PRIMARY
 
 function updateSteps() {
+
+    function updateButton(button, confirm, text = '') {
+        if (confirm) {
+            button.style.border = '0.1em solid  var(--device-found-color)';
+            button.style.color = 'var(--device-found-color)';
+        } else {
+            button.style.border = '0.1em solid var(--searching-device-color)';
+            button.style.color = 'var(--searching-device-color)';
+        }
+
+        if (text != '') {
+            button.innerHTML = '<div class="nouserselect">' + text + '</div>';
+        }
+    }
+
+
     currentStep = 1;
 
+    var deviceStatusContainerDiv = document.getElementById('devicestatuscontainerdiv');
+    var deviceStatusDiv = document.getElementById('devicestatusdiv');
+    var deviceProgressDiv = document.getElementById('devicestatusprogressdiv');
+
+    var payloadButton = document.getElementById('payloadbutton');
+
     if (validateDevice()) {
+        updateButton(deviceStatusContainerDiv, true);
+        deviceStatusDiv.innerHTML = 'A Switch in RCM mode has been found';
+        deviceProgressDiv.style.display = 'none';
         currentStep = 2;
-    } 
+    } else {
+        updateButton(deviceStatusContainerDiv, false);
+        deviceStatusDiv.innerHTML = 'Now searching for a Switch in RCM mode';
+        deviceProgressDiv.style.display = 'inline';
+    }
+
     if (validatePayload()) {
+        updateButton(payloadButton, true, payloadPath);
+        deviceStatusDiv.innerHTML = '<div class="nouserselect">A switch in RCM mode has been found</div>';
         currentStep = 3;
+    } else {
+        updateButton(payloadButton, false, 'Select a payload .bin file');
     }
 
     for (var i = 1; i < 4; i++) {
         var instructionID = 'i' + i.toString();
+        var currentInstructionDiv = document.getElementById(instructionID);
+
         if (i == currentStep) {
-            document.getElementById(instructionID).classList.remove('fade-out');
-            document.getElementById(instructionID).classList.add('fade-in');
+            currentInstructionDiv.classList.remove('fade-out');
+            currentInstructionDiv.classList.add('fade-in');
+            currentInstructionDiv.classList.remove('nouserselect');
+            currentInstructionDiv.style.pointerEvents = 'auto';
         } else {
-            document.getElementById(instructionID).classList.add('fade-out')
-            document.getElementById(instructionID).classList.remove('fade-in');
+            currentInstructionDiv.classList.add('fade-out')
+            currentInstructionDiv.classList.remove('fade-in');
+            currentInstructionDiv.classList.add('nouserselect');
+            currentInstructionDiv.style.pointerEvents = 'none';
         }
     }
 }
+
 
 async function launchPayload() {
     if (!validateDevice()) {
@@ -123,6 +266,21 @@ async function launchPayload() {
 
     if (!validatePayload()) {
         console.log('The selected payload path is invalid, or the payload is broken... Cannot launch payload.');
+        return;
+    }
+
+    const os = require('os');
+    if (os.type() == 'Windows_NT') {
+        const path = require('path');
+        const { exec } = require('child_process');
+        const smashProcess = exec(path.join(__dirname, 'TegraRcmSmash.exe ' + payloadPath), function (error, stdout, stderr) { });
+        smashProcess.on('exit', function (code) {
+            if (code == 0) {
+                smashCompleteDialog(true);
+            } else {
+                smashCompleteDialog(false);
+            }
+        });
         return;
     }
 
@@ -167,9 +325,11 @@ async function launchPayload() {
             request: 0x00,
             value: 0x00,
             index: 0x00
-        }, vulnerabilityLength);   
+        }, vulnerabilityLength);
+        smashCompleteDialog(true);
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        smashCompleteDialog(false);
     }
 }
 
@@ -179,16 +339,16 @@ async function loadDevice() {
         device = await USB.requestDevice({ filters: [{ vendorId: 0x0955 }] });
         updateSteps();
     } catch (error) {
-        console.log(error);
+        //console.log(error);
         device = null;
         updateSteps();
         return;
     }
 }
 
-async function selectPayload() { 
+async function selectPayload() {
     payloadPath = '';
-    const {ipcRenderer} = require('electron')
+    const { ipcRenderer } = require('electron')
     ipcRenderer.send('show-open-dialog')
 
     ipcRenderer.on('fileSelected', (event, arg) => {
