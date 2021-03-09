d636bd6c513fa2f5541e6b2bd8f07f3d227ca81f
Tom Taylor <tom@tomxxi.com>
2021-03-09 01:39:45 +0000

    Cleanup

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-var main_1 = __importDefault(require("./main"));
-main_1.default.main(electron_1.app, electron_1.BrowserWindow);
